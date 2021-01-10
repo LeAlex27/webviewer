0e07a64910e16376747a45b40cc07d5f1c6ff02a
Lennart Husvogt <lennart@husvogt.net>
2021-01-10 15:00:51 -0500

    some minor changes

--- setup.py
+++ setup.py
@@ -1,30 +1,34 @@
 import subprocess
 from setuptools import setup, find_packages, Command
 
+from webviewer import __version__
+
 
 class RccCommand(Command):
     description = "compile Qt resources"
     user_options = []
 
     def initialize_options(self):
-        self.packages = []
+        # todo: clean this up
+        # self.packages = []
+        pass
 
     def finalize_options(self):
         pass
 
     def run(self):
-        for package in self.packages:
-            print(package)
+        # for package in self.packages:
+        #    print(package)
 
         subprocess.run(['rcc', '-o', 'webviewer/resources.py', '-g', 'python', 'webviewer/resources.qrc'])
 
 
 setup(name="webviewer",
-      version="0.1",
+      version=__version__,
       author="Lennart Husvogt",
       author_email="lennart@husvogt.net",
       url="https://github.com/LeAlex27/webviewer",
       packages=find_packages(),
       python_requires=">=3.7",
-      install_requires=["PySide2"],
+      install_requires=["PySide2>=5.14"],
       cmdclass={'rcc': RccCommand})
--- webviewer/__main__.py
+++ webviewer/__main__.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import sys
 
 from PySide2.QtCore import Qt, QUrl, QCommandLineParser, QSettings
