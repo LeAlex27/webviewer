3f067ea3e0bfe04d17d861d64d506e6a550b6b02
Lennart Husvogt <lennart@husvogt.net>
2022-02-07 13:41:12 -0500

    removed RccCommand

--- setup.py
+++ setup.py
@@ -1,26 +1,7 @@
 import subprocess
-from setuptools import setup, find_packages, Command
+from setuptools import setup, find_packages
 
 from webviewer import __version__
-
-
-class RccCommand(Command):
-    description = "compile Qt resources"
-    user_options = []
-
-    def initialize_options(self):
-        # todo: clean this up
-        # self.packages = []
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        # for package in self.packages:
-        #    print(package)
-
-        subprocess.run(['rcc', '-o', 'webviewer/resources.py', '-g', 'python', 'webviewer/resources.qrc'])
 
 
 setup(name="webviewer",
@@ -30,5 +11,4 @@ setup(name="webviewer",
       url="https://github.com/LeAlex27/webviewer",
       packages=find_packages(),
       python_requires=">=3.7",
-      install_requires=["PySide6>=6.2"],
-      cmdclass={'rcc': RccCommand})
+      install_requires=["PySide6>=6.2"])
